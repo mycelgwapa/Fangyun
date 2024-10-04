fe0a6ef9cea75e0ae02641f6770dc840b0d181be
Agustin Quirante <156905174+mycelgwapa@users.noreply.github.com>
2024-10-04 03:23:27 +0000

    deploy

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const server = require('./public/server.js');
-const bot = require('./src/bot');
 
-bot.launch()
-.then(() => console.log('Bot is running...'))
-.catch(err => console.error('Error launching the bot:', err));
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 server.listen(8769, () => {
   console.log(`Working at 9to5`);
